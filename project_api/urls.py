10f94895dad38094bbd41e5fa68c20475867ffae
August Eilertsen <augustle@stud.ntnu.no>
2020-03-10 11:20:17 +0100

    fix

--- project_api/urls.py
+++ project_api/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from django.urls import path
 from django.conf.urls import url, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('admin/', admin.site.urls),
     url(r'^', include('user.urls')),
     url(r'^', include('data.urls')),
     url(r'^', include('student.urls')),
