4eae7accc0ff79d6732a0bce93105b121264415b
August Eilertsen <augustle@stud.ntnu.no>
2020-03-22 17:52:01 +0100

    cors stuff

--- project_api/settings.py
+++ project_api/settings.py
@@ -51,8 +51,8 @@ CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000",
     "http://127.0.0.1:8000",
     'https://teamaccelerator.idi.ntnu.no',
-    "https://teamaccelerator.surge.sh"
-
+    "https://teamaccelerator.surge.sh",
+    "https://teamacceleratoreu.herokuapp.com"
 ]
 
 ROOT_URLCONF = 'project_api.urls'
